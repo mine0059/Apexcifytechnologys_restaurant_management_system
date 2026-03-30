c976899184ea1c31587e00e7938cb42fd3d4aa8a
mine0059 <onojefemuemine@gmail.com>
2026-03-30 10:38:29 +0100

    add: index route for v1

---
+++ src/routes/v1/index.ts
@@ -0,0 +1,26 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import { Router } from "express";
+
+const router = Router();
+
+// Routes
+import authRoutes from '@/routes/v1/auth';
+
+// Root route
+router.get('/', (_, res) => {
+    res.status(200).json({
+        message: "API is live",
+        status: 'Ok',
+        version: '1.0.0',
+        docs: '',
+        timestamp: new Date().toISOString(),
+    });
+});
+
+router.use('/auth', authRoutes);
+
+export default router;
