112def18174438682d89601e687f8725eb54ec81
mine0059 <onojefemuemine@gmail.com>
2026-03-30 10:38:08 +0100

    add: auth models

---
+++ src/models/user.ts
@@ -0,0 +1,69 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface IUser {
+    username: string;
+    email: string;
+    password: string;  
+    role: 'admin' | 'user'; 
+    firstName?: string;
+    lastName?: string;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: [true, 'Username is required'],
+            maxLength: [20, 'Username must be less than 20 characters'],
+            unique: [true, 'Username must be unique'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            maxLength: [50, 'Email must be less than 50 characters'],
+            unique: [true, 'Email must be unique'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            select: false,
+        },
+        role: {
+            type: String,
+            required: [true, 'Role is required'],
+            enum: {
+                values: ['admin', 'user'],
+                message: '{VALUE} is not supported',
+            },
+            default: 'user',
+        },
+        firstName: {
+            type: String,
+            maxLength: [20, 'First name must be less than 20 characters'],
+        },
+        lastName: {
+            type: String,
+            maxLength: [20, 'Last name must be less than 20 characters'],
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) {
+        return;
+    }
+
+    // Hash the Password
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+export default model<IUser>('User', userSchema);
